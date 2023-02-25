ea77a082391ded2d6029107ccfac4aba3c64b797
Vivek kumar <vivekkumar@Viveks-MacBook-Pro.local>
2023-02-25 11:11:26 +0530

    ok

---
+++ src/components/About.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Route } from 'react-router'
+
+function About() {
+    return (
+        <div className="content-section">
+        <div className="title">
+          <h1 className="first"> About Us</h1>
+        </div>
+        <div className="image-section">
+          <img src="https://i.pinimg.com/originals/c9/12/d4/c912d49f5f63e3c25aae2465f7577e7a.png" alt="LPU logo" />
+        </div>
+        <div className="content">
+          <h3>An Educational Initiative</h3>
+          <p>
+            We, the students of CSE Branch of Lovely Professional University, are here to explore the
+            world of education. The education world is a type of dream world
+            where we can study without any boundings or
+            hesitation. Along with the proficient knowledge of class 12th, we are
+            exploring the world of other classes and trying best to be good at
+            it.
+          </p>
+          <p>
+            This website is created by 
+            <a href="#" target="_blank"> Sanjay Kumar</a>,
+            <a href="#" target="_blank"> Ridhi Soni</a>,
+            <a href="#" target="_blank"> Vivek Kumar</a>, <a href="#" target="_blank"> Harsh Raj</a>,
+            <a href="#" target="_blank"> Rounak Raghuwanshi</a>
+          </p>
+        </div>
+      </div>
+    )
+}
+
+export default About
---
+++ src/components/Team_mem.js
@@ -0,0 +1,24 @@
+const Team_mem = [
+    {
+        id:1,
+        Name:"Sanjay Kumar",
+    },
+    {
+        id:2,
+        Name:"Ridhi Soni",
+    },
+    {
+        id:3,
+        Name:"Vivek Kumar",
+    },
+    {
+        id:4,
+        Name:"Harsh Raj",
+    },
+    {
+        id:5,
+        Name:"Rounak Raghuwanshi",
+    }
+    
+]
+export default Team_mem
